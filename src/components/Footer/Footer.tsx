ca2637fbe702d0f16a78c719529154ca270eedba
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-04-09 02:36:58 +0300

    addcomit_09.04.2025
    Попрацював з дизайном хедеру, ризиків, бенефітів, футеру, сайт почав виглядати більш приємним для ока, функціональним та інтерактивним. Додано багато анімацій, підібрано поєднання кольорів.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,48 +1,67 @@
 import style from "./Footer.module.css";
-import { IoLocationOutline } from "react-icons/io5";
-import { FaPhoneAlt } from "react-icons/fa";
+import { IoLocationOutline, IoTimeOutline } from "react-icons/io5";
+import { FaPhoneAlt, FaTelegramPlane } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
-import { BsInstagram } from "react-icons/bs";
-import { FaTelegramPlane } from "react-icons/fa";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <div className={style.info}>
-        <IoLocationOutline className={style.icon} />
-        <a>Розташування: Київ</a>
+      <div className={style.footerContainer}>
+        {/* Блок контактів */}
+        <div className={style.contactBlock}>
+          <h3 className={style.blockTitle}>Контакти</h3>
+          <div className={style.contactItem}>
+            <IoLocationOutline className={style.icon} />
+            <span>м. Київ, вул. Прикладна, 12</span>
+          </div>
+          <div className={style.contactItem}>
+            <IoTimeOutline className={style.icon} />
+            <span>Пн-Пт: 9:00-18:00</span>
+          </div>
+          <div className={style.contactItem}>
+            <FaPhoneAlt className={style.icon} />
+            <a href="tel:+380955144040">+38 (095) 514-40-40</a>
+          </div>
+          <div className={style.contactItem}>
+            <MdOutlineEmail className={style.icon} />
+            <a href="mailto:ruslan_naumenko@gmail.com">ruslan_naumenko@gmail.com</a>
+          </div>
+        </div>
+
+        <div className={style.socialBlock}>
+          <h3 className={style.blockTitle}>Соціальні мережі</h3>
+          <div className={style.socialLinks}>
+            <a href="https://www.instagram.com/NR_car" target="_blank" rel="noopener noreferrer" className={style.socialLink}>
+              <BsInstagram className={style.socialIcon} />
+              <span>Instagram</span>
+            </a>
+            <a href="https://t.me/your_channel" target="_blank" rel="noopener noreferrer" className={style.socialLink}>
+              <FaTelegramPlane className={style.socialIcon} />
+              <span>Telegram</span>
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className={style.socialLink}>
+              <BsFacebook className={style.socialIcon} />
+              <span>Facebook</span>
+            </a>
+          </div>
+        </div>
+
+        <div className={style.navBlock}>
+          <h3 className={style.blockTitle}>Навігація</h3>
+          <nav className={style.navLinks}>
+            <a href="/" className={style.navLink}>Головна</a>
+            <a href="/services" className={style.navLink}>Послуги</a>
+            <a href="/about" className={style.navLink}>Про нас</a>
+            <a href="/blog" className={style.navLink}>Блог</a>
+            <a href="/contacts" className={style.navLink}>Контакти</a>
+          </nav>
+        </div>
       </div>
-      <div className={style.info}>
-        <FaPhoneAlt className={style.icon} />
-        <a href="tel:+380955144040" title="Зателефонуйте нам">
-          Тел. +380955144040
-        </a>
-      </div>
-      <div className={style.info}>
-        <MdOutlineEmail className={style.icon} />
-        <a href="mailto:ruslan_naumenko@gmail.com">ruslan_naumenko@gmail.com</a>
-      </div>
-      <div className={style.info}>
-        <BsInstagram className={style.icon} />
-        <a
-          href="https://www.instagram.com/your_profile"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Instagram"
-        >
-          NR_car
-        </a>
-      </div>
-      <div className={style.info}>
-        <FaTelegramPlane className={style.icon} />
-        <a
-          href="https://t.me/your_channel"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Telegram"
-        >
-          Руслан Науменко
-        </a>
+
+      {/* Копірайт */}
+      <div className={style.copyright}>
+        <p>© {new Date().getFullYear()} NR Car. Всі права захищені</p>
       </div>
     </footer>
   );
