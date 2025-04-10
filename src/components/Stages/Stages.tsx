600523d120fa5e2ed8b23f8d539912e95880a6dd
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-04-10 17:34:38 +0300

    addcomit_17.37_10.04.2025
    Додано дизайн секції Автопідбір під ключ.

--- src/components/Stages/Stages.tsx
+++ src/components/Stages/Stages.tsx
@@ -1,67 +1,77 @@
-import style from './Stages.module.css';
+import { useState } from "react";
+import style from "./Stages.module.css";
+import { TfiArrowCircleDown } from "react-icons/tfi";
+import { TfiArrowCircleUp } from "react-icons/tfi";
 
 export default function Stages() {
+  const [expandedStages, setExpandedStages] = useState<number[]>([]);
 
-    return (
-        <div className={style.wrapperForStages}>
-            <h4 className={style.titleForStages}>Автопідбір під ключ</h4> 
-            <ul className={style.listForStages}>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>1. Консультація та визначення критеріїв</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                       Першим етапом є визначення бажаного автомобіля або декількох моделей,
-                       що підходять за вашими критеріями. Оговорюємо бюджет, допустимі роки випуску автомобіля,
-                       пробіг та комплектацію. Також оговорюємо ціну за послугу автопідбору під ключ.
-                    </p>
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>2. Підписання договору</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                        Після узгодження всіх вимог до бажаного авто та ціни за послугу
-                        зустрічаємся та заключаємо договір про надання послуг автопідбору.
-                    </p>
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>3. Пошук варіантів</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                        Здійснюється аналіз всіх авто, які підходять за вашими критеріями та відсіюються
-                        ризиковані варіанти. Найбільш привабливі автівки відбираються та надсилаються на узгодження.
-                    </p>
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>4. Онлайн-огляд та узгодження</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                        Якщо одна з надісланих автівок вам подобається то комунікуємо з продавцем та узгоджуємо виїздний огляд.
-                       Якщо ні, то продовжуємо пошук авто, яке буде більш підходящим для вас.
-                    </p>
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>5. Виїзний огляд найкращих варіантів</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                        На виїздній діагностиці перевіряємо лакофарбове покриття, геометрію, стан салону,
-                        робимо комп'ютерну діагностику, визначаємо пробіг, помилки, наявність подушок безпеки.
-                        Робиться фото та відео звіт про стан авто та його недоліки, а також висновок про те
-                        чи варто розглядати дане авто для його діагностики на СТО.
-                    </p>
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>6. Детальна діагностика на СТО</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                        В залежності від авто та ваших уподобань домовляємося з необхідним СТО де більш детально робимо огляд
-                        ходової частини автомобіля, ендоскопію для встановлення стану циліндрів двигуна, також механіки дають
-                        своє заключення про необхідність заміни тих чи інших деталей автівки. На даному етапі ви також
-                        отримуєте звіт про виконану роботу в фото та відео форматі.  </p> 
-                </li>
-                <li className={style.pointOfListForStages}>
-                    <strong className={style.titleOfPointOfListForStages}>7. Супровід угоди</strong>
-                    <p className={style.textOfPointOfListForStages}>
-                       Якщо авто проходить СТО та відповідає вашим параметрам, відбуваються торги з продавцем, виходячи з
-                       реальної ринкової вартості авто, його стану та виявлених недоліків домовляємося про ціну. Після узгодження
-                       ціни оранізовуємо переоформлення автомобіля на МРЕО. </p>
-                </li>
-            </ul>
-            
-        </div>
-    );
-    
+  const toggleStage = (index: number) => {
+    if (expandedStages.includes(index)) {
+      setExpandedStages(expandedStages.filter((i) => i !== index));
+    } else {
+      setExpandedStages([...expandedStages, index]);
+    }
+  };
+
+  const stages = [
+    {
+      title: "1. Консультація та визначення критеріїв",
+      text: "Першим етапом є визначення бажаного автомобіля або декількох моделей, що підходять за вашими критеріями. Оговорюємо бюджет, допустимі роки випуску автомобіля, пробіг та комплектацію. Також оговорюємо ціну за послугу автопідбору під ключ."
+    },
+    {
+      title: "2. Підписання договору",
+      text: "Після узгодження всіх вимог до бажаного авто та ціни за послугу зустрічаємся та заключаємо договір про надання послуг автопідбору."
+    },
+    {
+      title: "3. Пошук варіантів",
+      text: "Здійснюється аналіз всіх авто, які підходять за вашими критеріями та відсіюються ризиковані варіанти. Найбільш привабливі автівки відбираються та надсилаються на узгодження."
+    },
+    {
+      title: "4. Онлайн-огляд та узгодження",
+      text: "Якщо одна з надісланих автівок вам подобається то комунікуємо з продавцем та узгоджуємо виїздний огляд. Якщо ні, то продовжуємо пошук авто, яке буде більш підходящим для вас."
+    },
+    {
+      title: "5. Виїзний огляд найкращих варіантів",
+      text: "На виїздній діагностиці перевіряємо лакофарбове покриття, геометрію, стан салону, робимо комп'ютерну діагностику, визначаємо пробіг, помилки, наявність подушок безпеки. Робиться фото та відео звіт про стан авто та його недоліки, а також висновок про те чи варто розглядати дане авто для його діагностики на СТО."
+    },
+    {
+      title: "6. Детальна діагностика на СТО",
+      text: "В залежності від авто та ваших уподобань домовляємося з необхідним СТО де більш детально робимо огляд ходової частини автомобіля, ендоскопію для встановлення стану циліндрів двигуна, також механіки дають своє заключення про необхідність заміни тих чи інших деталей автівки. На даному етапі ви також отримуєте звіт про виконану роботу в фото та відео форматі."
+    },
+    {
+      title: "7. Супровід угоди",
+      text: "Якщо авто проходить СТО та відповідає вашим параметрам, відбуваються торги з продавцем, виходячи з реальної ринкової вартості авто, його стану та виявлених недоліків домовляємося про ціну. Після узгодження ціни оранізовуємо переоформлення автомобіля на МРЕО."
+    }
+  ];
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.wrapperForStages}>
+        <h4 className={style.titleForStages}>Автопідбір під ключ</h4>
+        <ul className={style.listForStages}>
+          {stages.map((stage, index) => (
+            <li key={index} className={style.pointOfListForStages}>
+              <div 
+                className={style.titleOfPointOfListForStages} 
+                onClick={() => toggleStage(index)}
+              >
+                <strong className={style.title}>{stage.title}</strong>
+                {expandedStages.includes(index) ? (
+                    <TfiArrowCircleUp className={style.icon} />
+                ) : (
+                    <TfiArrowCircleDown className={style.icon}/>
+                )}
+              </div>
+              {expandedStages.includes(index) && (
+                <p className={style.textOfPointOfListForStages}>
+                  {stage.text}
+                </p>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
