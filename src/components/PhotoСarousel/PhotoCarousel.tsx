474124db654c6a533ff6bcc7b78ca52454fbe55c
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-04-27 23:54:50 +0300

    addcomit_23.54_27.04.2025
    Доданий слайдер з історіями автопідбору, використоно css.

--- src/components/PhotoСarousel/PhotoCarousel.tsx
+++ src/components/PhotoСarousel/PhotoCarousel.tsx
@@ -1,26 +1,97 @@
 import style from "./PhotoCarousel.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { PropagateLoader } from "react-spinners";
+import photoData from "./Photo.json";
+import type { MouseEvent } from "react";
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { FiArrowRightCircle } from "react-icons/fi";
+
+interface PhotoItem {
+  id: number;
+  photo: string;
+  coment?: string;
+  car?: string;
+}
 
 export default function PhotoCarousel() {
-  const [numOfFoto, setNumOfFoto] = useState(1);
+  const [numOfFoto, setNumOfFoto] = useState<number>(1);
+  const [photoArray, setPhotoArray] = useState<PhotoItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  function hendlerPlus() {
-    return setNumOfFoto(numOfFoto + 1);
+  function hendlerPlus(e: MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault();
+    if (numOfFoto <= photoArray.length) setNumOfFoto((prev) => prev + 1);
   }
 
-  function hendlerMinus() {
-    return setNumOfFoto(numOfFoto - 1);
+  function hendlerMinus(e: MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault();
+    if (numOfFoto >= 1) setNumOfFoto((prev) => prev - 1);
   }
 
-  console.log(numOfFoto);
+  useEffect(() => {
+    setPhotoArray(photoData);
+    setIsLoading(false);
+  }, []);
+
+  const currentPhoto = photoArray.find((item) => item.id === numOfFoto);
 
   return (
-    <div>
+    <>
       <div className={style.galery}>
-             
+        {isLoading ? (
+          <PropagateLoader
+            size={"15px"}
+            color={"#3cc0ef"}
+            speedMultiplier={0.5}
+          />
+        ) : (
+          <div className={style.blockOfPhotoAndComent}>
+            <h5 className={style.headerMyCarSearch}>Мої автопідбори</h5>
+            <p className={style.markAndModelCar}>{currentPhoto?.car}</p>
+            <div className={style.imgAndBtns}>
+              {numOfFoto > 1 ? (
+                <button
+                  onClick={hendlerMinus}
+                  className={style.buttonForGalerySwitch}
+                >
+                  <FiArrowLeftCircle className={style.svgForGalery} />
+                </button>
+              ) : (
+                <button
+                  onClick={hendlerMinus}
+                  className={style.buttonForGalerySwitch}
+                  disabled
+                >
+                  <FiArrowLeftCircle className={style.svgForGaleryDis} />
+                </button>
+              )}
+              <img
+                src={currentPhoto?.photo}
+                alt="Тут мало бути фото авто"
+                className={style.galeryImg}
+              />
+              {numOfFoto < 10 ? (
+                <button
+                  onClick={hendlerPlus}
+                  className={style.buttonForGalerySwitch}
+                >
+                  {" "}
+                  <FiArrowRightCircle className={style.svgForGalery} />{" "}
+                </button>
+              ) : (
+                <button
+                  onClick={hendlerPlus}
+                  className={style.buttonForGalerySwitch}
+                  disabled
+                >
+                  <FiArrowRightCircle className={style.svgForGaleryDis} />
+                </button>
+              )}
+            </div>
+          </div>
+        )}
       </div>
-      {numOfFoto > 1 ? <button onClick={hendlerMinus}> -</button> : <button onClick={hendlerMinus} disabled> -</button>}
-      {numOfFoto < 10 ? <button onClick={hendlerPlus}> + </button> : <button onClick={hendlerPlus} disabled> + </button>}
-    </div>
+      <p className={style.history}>{currentPhoto?.coment}</p>
+    </>
   );
 }
